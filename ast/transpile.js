d66dafb4d9189de4fccc3ea8b91a5704d168bba9
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2025-02-10 10:32:45 +0000

    transpile added try catch

--- ast/transpile.js
+++ ast/transpile.js
@@ -4,7 +4,13 @@ const fs = require('fs/promises');
 
 module.exports = async function transpile(inputFile, outputFile) {
   let input = await fs.readFile(inputFile, 'utf-8')
-  const ast = p.parse(input);
+  let ast = null;
+  try {
+    ast = p.parse(input);
+  } catch (e) {
+    console.error(e.message);
+    return;
+  }
   const output = escodegen.generate(ast);
   if (outputFile === undefined) {
       console.log(output);
