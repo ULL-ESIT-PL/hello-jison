2f7cbd4b26d31029fcf5774526e24f66abd1ca12
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2025-02-17 07:49:40 +0000

    yaml

--- embedded-lexer/use.js
+++ embedded-lexer/use.js
@@ -1,12 +1,17 @@
 #!/usr/bin/env node
 const YAML = require('json-to-pretty-yaml');
 const p = require("./embedded_lexer").parser;
-
+// Remaining arguments are keys to omit from the output
 let omit = new Set(process.argv.slice(3)); // Example: use.js 2+1 loc value
 
 function replace(key, value) {
     if (omit.has(key)) return undefined;
     return value;
+}
+
+function yaml2string(t, replace) {
+  let result = JSON.parse(JSON.stringify(t, replace, 2));
+  return YAML.stringify(result);
 }
 
 let input = process.argv[2] || "2+1";
@@ -15,9 +20,7 @@ try {
   p.yy = {E: "Expression", T: "Term", NAT: "Number", "+": "Plus"};
   const t = p.parse(input);
   console.error("Parse Tree:\n")
-  let result = JSON.stringify(t, replace, 2);
-  pt = JSON.parse(result);
-  console.log(YAML.stringify(pt));
+  console.log(yaml2string(t, replace));
 } catch (e) {
   console.error(e.message);
 }
